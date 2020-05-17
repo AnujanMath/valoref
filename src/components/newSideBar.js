30f2e510ca2374a8ecc1e6e0b51a7f0ffe35d539
Anujan <anujanftw@gmail.com>
2020-05-17 12:34:07 -0400

    Need to fix scrolling, added list headers

--- src/components/newSideBar.js
+++ src/components/newSideBar.js
@@ -1,46 +1,64 @@
-import React from 'react';
-import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import { makeStyles, Box } from '@material-ui/core';
+import React from "react"
+import Drawer from "@material-ui/core/Drawer"
+import List from "@material-ui/core/List"
+import Divider from "@material-ui/core/Divider"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import { makeStyles, Box } from "@material-ui/core"
+import ListSubheader from "@material-ui/core/ListSubheader"
 
 const useStyles = makeStyles({
   paper: {
     left: 70,
-    backgroundColor: '#16191C',
-    boxShadow: '4px 0px 20px'
-  }
-});
-const maps = ['Haven', 'Bind', 'Split'];
+    backgroundColor: "#16191C",
+    boxShadow: "4px 0px 20px",
+  },
+})
+const maps = ["Haven", "Bind", "Split"]
 
-const characters = ['Breach', 'Brimstone', 'Cypher', 'Jett', 'Omen', 'Phoenix', 'Raze', 'Sage', 'Sova', 'Viper']
+const characters = [
+  "Breach",
+  "Brimstone",
+  "Cypher",
+  "Jett",
+  "Omen",
+  "Phoenix",
+  "Raze",
+  "Sage",
+  "Sova",
+  "Viper",
+]
 
 export default function SideBar({ showSideBar }) {
   console.log(showSideBar)
   const [state, setState] = React.useState({
-    left: false
-  });
-  const classes = useStyles();
+    left: false,
+  })
+  const classes = useStyles()
 
-  const toggleDrawer = () => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
+  const toggleDrawer = () => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return
     }
 
-    setState({ state, left: !state.left });
-  };
+    setState({ state, left: !state.left })
+  }
   const list = () => (
-
-    <Box
-      style={{ width: 250, color: 'white' }}
-    >
-      <List >
+    <Box style={{ width: 250, color: "white" }}>
+      <List
+        subheader={
+          <ListSubheader
+            style={{ color: "#A3A3A3" }}
+            component="div"
+            id="nested-list-subheader"
+          >
+            Select a map
+          </ListSubheader>
+        }
+      >
         {maps.map((text, index) => (
           <ListItem button key={text}>
             <ListItemText primary={text} />
@@ -48,7 +66,17 @@ export default function SideBar({ showSideBar }) {
         ))}
       </List>
       <Divider />
-      <List>
+      <List
+        subheader={
+          <ListSubheader
+            style={{ color: "#A3A3A3" }}
+            component="div"
+            id="nested-list-subheader"
+          >
+            Select an agent
+          </ListSubheader>
+        }
+      >
         {characters.map((text, index) => (
           <ListItem button key={text}>
             {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
@@ -57,7 +85,7 @@ export default function SideBar({ showSideBar }) {
         ))}
       </List>
     </Box>
-  );
+  )
 
   return (
     <React.Fragment>
@@ -65,12 +93,13 @@ export default function SideBar({ showSideBar }) {
       <Drawer
         BackdropProps={{ invisible: true }}
         classes={{ paper: classes.paper }}
-        style={{ postion: 'absolute', left: 100 }}
+        style={{ postion: "absolute", left: 100 }}
         open={showSideBar}
         variant="persistent"
-        onClick={toggleDrawer()} >
+        onClick={toggleDrawer()}
+      >
         {list()}
       </Drawer>
     </React.Fragment>
-  );
+  )
 }
