bbdfe887096ccaa448213a773be2febf27fd8ab1
Anujan <anujanftw@gmail.com>
2020-05-19 14:29:56 -0400

    idk what this change is tbh

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ import { createStore } from "redux"
 import SwitchListSecondary from "../components/main-content/switchBoard.js"
 
 import { Provider, connect } from "react-redux"
-const countReducer = function (state = true, action) {
+const countReducer = function (state = true, action) { //how do we make state support more than just one 
   switch (action.type) {
     case "toggle":
-      return !state 
+      return !state
     default:
       return state
   }
@@ -20,16 +20,16 @@ const mapStateToProps = state => {
 const Component = ({ count, handleToggle }) => (
   <div>
     <h1>Helloworld React & Redux! {count ? "10" : "5"}</h1>
-    <button  onClick={handleToggle}>Toggle meeeeeeeeeeeeeeee</button>
-    <SwitchListSecondary toggle={count}/>
+    <button onClick={handleToggle}>Toggle meeeeeeeeeeeeeeee</button>
+    <SwitchListSecondary toggle={count} />
   </div>
 )
 const mapDispatchToProps = dispatch => {
-    return {
-      handleToggle: () => dispatch({ type: 'toggle' }),
-    }
-  };
-  const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+  return {
+    handleToggle: () => dispatch({ type: "toggle" }),
+  }
+}
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component)
 let store = createStore(countReducer)
 
 export default function IndexPage() {
