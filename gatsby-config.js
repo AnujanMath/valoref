f15416c6215249e0001f5fb7707a8a0791eaaf47
Anujan <anujanftw@gmail.com>
2020-05-21 15:42:07 -0400

    ok cloudinary works take a look at localhost/cloudinary.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,18 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    menuLinks:[
+    menuLinks: [
       {
-        name:'home',
-        link:'/'
+        name: "home",
+        link: "/",
       },
       {
-        name:'contact us',
-        link:'/page-2'
-      }
+        name: "contact us",
+        link: "/page-2",
+      },
     ],
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
@@ -36,6 +39,24 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-transformer-cloudinary",
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        uploadFolder: "gatsby-cloudinary",
+      },
+    },
+    {
+      resolve: `gatsby-source-cloudinary`,
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        resourceType: `image`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
